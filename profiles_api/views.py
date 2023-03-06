84a318dca76974c8fa55e1204bb1e3c1137604ab
LIKITH S <likiths2501@gmail.com>
2023-03-06 19:23:17 +0530

    Added searching to user profile API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -12,6 +12,7 @@ from rest_framework import viewsets
 from profiles_api import models
 from rest_framework.authentication import TokenAuthentication
 from profiles_api import permissions
+from rest_framework import filters
 
 
 # Create your views here.
@@ -118,3 +119,6 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    #search options
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
